71835a6898a042785ea18249d716ca2543f352e5
tech specialist <gr.heros@gmail.com>
2021-12-12 21:59:33 +0200

    Hospitals now ok

--- auth/routes/appointments.js
+++ auth/routes/appointments.js
@@ -11,9 +11,10 @@ const appointments = require('../model/appointment');
 
 
 router.get('/hospital',async(req,res) => {
-    const hospitals_list = await hospital.findOne({hospital_name:"General Regional Hospital Papageorgiou"});
+    const hospitals_list = await hospital.find().select({"hospital_name":1, "_id": 0});
     console.log(hospitals_list);
-    res.json({"hospital_name":hospitals_list.hospital_name});
+    res.send(hospitals_list)
+    //res.json({"hospital_name":hospitals_list.hospital_name});
 
 });
 router.get('/doctor',async(req,res) => {
@@ -26,7 +27,7 @@ router.get('/doctor',async(req,res) => {
 });
 router.get('/date',async(req,res) => {
     console.log(req.body.fullname_doc)
-    const vv = req.body.hospital_name
+    const vv = req.body.fullname_doc
     const doctors_list = await doctors.findOne({"hosp_name_doc":req.body.hospital_name});
     console.log(doctors_list.fullname_doc);
     res.json({"fullname_doc":doctors_list.fullname_doc,"medic_spec":doctors_list.medic_spec});
